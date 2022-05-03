1266c9f815affde364cdb6cbc87e291513b31314
Kadanin <11957895+kadanin@users.noreply.github.com>
2022-05-03 22:17:27 +0300

    dev

--- tests/cases/MainTest.php
+++ tests/cases/MainTest.php
@@ -12,15 +12,8 @@ class MainTest extends TestCase
 
     public function testMain(): void
     {
-        static::assertEquals('SELECT * FROM `test` `t` WHERE `t`.`id` = 1', Test::find()->byID(1)->alias('t')->rawSql($this->db()));
+        static::assertEquals('SELECT * FROM `test` `t` WHERE `t`.`id` = 1', Test::find()->byID(1)->alias('t')->rawSql());
         static::assertEquals(['id' => 1, 'text' => 'text1'], Test::find()->orderBy(['id' => \SORT_ASC])->asArray()->alone());
         static::assertEquals([['id' => 2, 'text' => 'text2']], Test::find()->byID(2)->asArray()->all());
-    }
-
-    private function db(): Connection
-    {
-        $dir = \dirname(\realpath(__DIR__));
-        echo "sqlite:$dir/data/db.sqlite\n";
-        return $this->db ?? ($this->db = new Connection(['dsn' => 'sqlite:@app/tests/data/db.sqlite']));
     }
 }
