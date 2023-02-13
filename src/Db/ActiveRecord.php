9fc2458f00d552cfde8399ee0e6eca68526796e8
Kadanin Artyom <11957895+kadanin@users.noreply.github.com>
2023-02-13 08:16:22 +0300

    Relational IDE Helping Methods

--- src/Db/ActiveRecord.php
+++ src/Db/ActiveRecord.php
@@ -13,4 +13,32 @@ class ActiveRecord extends \yii\db\ActiveRecord
     {
         return Yii::createObject(ActiveQuery::class, [static::class]);
     }
+
+    /**
+     * Overriding this in PHPDoc helps IDE to detect resulting query
+     * This will not be typehinted for now in case of cross-db relations
+     *
+     * @param \yii\db\BaseActiveRecord $activeRecord
+     * @param array                    $link
+     *
+     * @return \yii\db\ActiveQueryInterface
+     */
+    public static function hasOneMe($activeRecord, array $link)
+    {
+        return $activeRecord->hasOne(static::class, $link);
+    }
+
+    /**
+     * Overriding this in PHPDoc helps IDE to detect resulting query
+     * This will not be typehinted for now in case of cross-db relations
+     *
+     * @param \yii\db\BaseActiveRecord $activeRecord
+     * @param array                    $link
+     *
+     * @return \yii\db\ActiveQueryInterface
+     */
+    public static function hasManyMe($activeRecord, array $link)
+    {
+        return $activeRecord->hasMany(static::class, $link);
+    }
 }
--- tests/classes/Test.php
+++ tests/classes/Test.php
@@ -7,6 +7,9 @@ use Kadanin\Yii2ArExt\Db\ActiveRecord;
 /**
  * @property string $id   [INTEGER]
  * @property string $text [TEXT]
+ *
+ * @method static TestQuery hasOneMe($activeRecord, array $link)
+ * @method static TestQuery hasManyMe($activeRecord, array $link)
  */
 class Test extends ActiveRecord
 {
